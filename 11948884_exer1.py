1da9d0f717a1601ce6b607aea47324699d80ec9f
Annika Alonzo <annika_alonzo@dlsu.edu.ph>
2020-01-26 22:01:51 +0800

    First commit

---
+++ 11948884_exer1.py
@@ -0,0 +1,17 @@
+##################################################
+
+# Author: Annika Alonzo
+# Date: Jan. 26, 2020
+# Course: LBYCPA1
+
+# Program description:
+# For laboratory activity 4, the function of the program is to present a selection screen to the user, there will be two selections, both are engineering 
+# equations.
+# There is also a quit option, the user may opt to choose 1, 2, or q for their selection. The program should then be able to show the user what thy have 
+# chosen.
+# Depending on the equation they have chosen, on the terminal it should allow the user to choose what variable they are calculating for in the equation,
+# ater choosing the unknown variable, they should be able to input the values of the other variables and then the program should be able to calculate 
+# for the unknown variable
+# After all of that, the user should be able to either go back to the selection screen or quit the program. 
+
+##################################################
